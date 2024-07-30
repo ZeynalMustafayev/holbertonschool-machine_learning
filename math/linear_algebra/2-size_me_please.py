7364a94878b4f197d03a2904354f0c95a2bea141
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-07-30 18:26:11 +0400

    Everything is documented

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""The shape should be returned as a list of integers"""
+
+
 def matrix_shape(matrix):
+    """Function that returns the shape of a matrix"""
     size = []
     while isinstance(matrix, list):
         size.append(len(matrix))
