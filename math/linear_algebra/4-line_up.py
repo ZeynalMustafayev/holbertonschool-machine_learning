ecaa4729ccaffcc5c35830d07914f9fae32edb58
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-01 18:59:30 +0400

    my version

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -6,4 +6,8 @@ def add_arrays(arr1, arr2):
     """Function that adds two arrays element-wise"""
     if len(arr1) != len(arr2):
         return None
-    return [arr1[i] + arr2[i] for i in range(len(arr1))]
+    array = []
+    for i in range(len(arr1)):
+        z = arr1[i] + arr2[i]
+        array.append(z)
+    return array
--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -2,12 +2,4 @@
 """Module that adds two 2D matrices"""
 
 
-def add_arrays(arr1, arr2):
-    """Function that adds two arrays element-wise"""
-    if len(arr1) != len(arr2):
-        return None
-    array = []
-    for i in range(len(arr1)):
-        z = arr1[i] + arr2[i]
-        array.append(z)
-    return array
+
