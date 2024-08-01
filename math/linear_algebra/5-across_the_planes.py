2133df7598f2af7f380870410abe2928f3c79983
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-01 18:57:59 +0400

    my version

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that adds two 2D matrices"""
+
+
+def add_arrays(arr1, arr2):
+    """Function that adds two arrays element-wise"""
+    if len(arr1) != len(arr2):
+        return None
+    array = []
+    for i in range(len(arr1)):
+        z = arr1[i] + arr2[i]
+        array.append(z)
+    return array
