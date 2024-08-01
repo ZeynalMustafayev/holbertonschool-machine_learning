32469711bf1e64f672fbb24bd1a1f83cb3d9d1cc
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-01 20:13:37 +0400

    I m going to sleep

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -2,4 +2,12 @@
 """Module that adds two 2D matrices"""
 
 
-
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2):
+        return None
+    array = []
+    for i in range(len(mat1)):
+        for j in range(len(mat1)):
+            z = mat1[i][j] + mat2[i][j]
+            array.append(z)
+        return array
