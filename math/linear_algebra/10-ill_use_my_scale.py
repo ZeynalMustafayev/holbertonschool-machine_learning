dc1a425b7ae0abbfeb27997bbee8730550dad39a
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-02 20:21:57 +0400

    easy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication"""
+
+
+def np_shape(matrix):
+    """Function that returns the shape of a numpy.ndarray"""
+    return matrix.shape
