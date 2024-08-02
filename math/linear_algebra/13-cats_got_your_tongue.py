ee51cdf18855163b19e274f625d5b45bea508520
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-02 21:04:26 +0400

    I forget not allowed thing

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -5,7 +5,4 @@ import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """Function that concatenates two numpy.ndarrays"""
-    if axis == 0:
-        return np.concatenate((mat1, mat2), axis=0)
-    else:
-        return np.concatenate((mat1, mat2), axis=1)
+    return np.concatenate((mat1, mat2), axis)
