15f02864f5558ef2d6035a36ca3fe462aa408845
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-02 20:00:27 +0400

    Ridin’ Bareback

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Function that performs matrix multiplication"""
+    new_matrix = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            mul = 0
+            for z in range(len(mat2)):
+                mul += mat1[i][z] * mat2[z][j]
+            row.append(mul)
+        new_matrix.append(row)
+    return new_matrix
