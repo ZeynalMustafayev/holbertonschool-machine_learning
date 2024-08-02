1e348d9ed85433d3ad8142b6138db304b7b7b3b2
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-02 18:47:43 +0400

    fix error

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def add_matrices2D(mat1, mat2):
     matrix = []
     for i in range(len(mat1)):
         row = []
-        for j in range(len(mat1)):
+        for j in range(len(mat1[0])):
             sum = mat1[i][j] + mat2[i][j]
             row.append(sum)
         matrix.append(row)
