c54acc23b21c3775ea2f47ecc068f9dca0b15286
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-02 20:34:00 +0400

    only transpose

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Function that returns the transpose of a numpy.ndarray"""
+    return matrix.transpose()
