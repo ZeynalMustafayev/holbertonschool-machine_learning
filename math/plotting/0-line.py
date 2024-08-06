be831de4a269be2ed9159af700d1f2102ce9dc44
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-06 15:45:00 +0400

    fix error

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,6 +8,6 @@ def line():
     """Line Plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
-    plt.plot(y, color='red')
+    plt.plot(y, 'r')
+    plt.xlim(0, 10)
     plt.show()
