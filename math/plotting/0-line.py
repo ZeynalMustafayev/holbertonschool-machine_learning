a3ae4b8e2993636e4c680feaf079bd228af561aa
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-06 15:40:33 +0400

    Line Graph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Line Plot"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Line Plot"""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, color='red')
+    plt.show()
