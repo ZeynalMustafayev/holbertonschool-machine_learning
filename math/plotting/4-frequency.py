8be1c3db5071daaf81662ef09a3dde01d6d91892
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-06 17:04:22 +0400

    fix error

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,5 +16,6 @@ def frequency():
     plt.title("Project A")
     plt.xlim(0, 100)
     plt.ylim(0, 30)
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor="black")
+    plt.xticks(np.arange(0, 110, 10))
     plt.show()
