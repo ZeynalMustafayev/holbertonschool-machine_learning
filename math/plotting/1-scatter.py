dc03840a31229b1a9846ed2ae2d623299df048c2
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-06 15:57:57 +0400

    interesting

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -11,9 +11,8 @@ def scatter():
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
-    plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, c='magenta', s=10)
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title('Men\'s Height vs Weight')
+    plt.scatter(x, y, c='magenta')
     plt.show()
