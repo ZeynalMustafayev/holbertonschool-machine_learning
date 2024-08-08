4d2633304c828ffff3b317e2a43e52b7b63c1b44
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-08 15:42:48 +0400

    fix documentation error

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Bars"""
+"""Bars function"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def bars():
-    """Bars"""
+    """Bars function"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
