b2790383a869dbf93a71d57d03203da41dead165
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-08 18:22:04 +0400

    fix error

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,7 +7,10 @@ def poly_integral(poly, C=0):
     if poly == [] or type(poly) is not list:
         return None
     if len(poly) == 1:
-        return [float(C)]
-    return [float(C)] + [float(poly[i] / (i + 1)) for i in range(len(poly))]
-poly = [5, 3, 0, 1]
-print(poly_integral(poly))
+        return [C]
+    integral = []
+    integral.append(C)
+    for i in range(len(poly)):
+        x = poly[i] / (i + 1)
+        integral.append(int(x) if x.is_integer() else x)
+    return integral
