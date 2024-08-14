62a45397e7c1907d1ec122adecf0bbdd9bed8ef4
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-14 17:51:13 +0400

    fix pyc error

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Depth of a decision tree"""
-
 import numpy as np
+
 
 class Node:
     """class node"""
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None,
+                 left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -19,20 +20,26 @@ class Node:
         if self.is_leaf:
             return self.depth
         else:
-            return max(self.left_child.max_depth_below(), self.right_child.max_depth_below())
+            return max(self.left_child.max_depth_below(),
+                       self.right_child.max_depth_below())
+
 
 class Leaf(Node):
+    """class leaf"""
     def __init__(self, value, depth=None):
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         return self.depth
 
+
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    """class decision tree"""
+    def __init__(self, max_depth=10, min_pop=1,
+                 seed=0, split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -45,5 +52,5 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
         return self.root.max_depth_below()
