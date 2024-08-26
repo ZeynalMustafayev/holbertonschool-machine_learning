244d654bbc85857c603dd0704d6974aa5ac21b27
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-26 20:39:27 +0400

    Neuron Forward Propagation

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""neuron class"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+    def __init__(self, nx):
+        """init function"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation function"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
