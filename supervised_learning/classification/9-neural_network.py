99eefa5d5a7757aca7329e46bfd6b6de9da360a7
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-08-27 20:42:30 +0400

    fix 9 and 10

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,53 +1,96 @@
 #!/usr/bin/env python3
-"""neural network with one hidden layer"""
+""" Task 9: 9. Privatize NeuralNetwork """
 import numpy as np
 
 
 class NeuralNetwork:
-    """neural network with one hidden layer"""
+    """
+    Defines a neural network with one hidden layer for performing
+    binary classification.
+    """
+
     def __init__(self, nx, nodes):
-        """constructor for class NeuralNetwork"""
-        if type(nx) is not int:
+        """
+        Initializes the neural network.
+        """
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(nodes) is not int:
+        if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        self.W1 = np.random.randn(nx, nodes).reshape(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(nodes).reshape(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
-    
+
+        self.__W1 = np.random.normal(0, 1, (nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(0, 1, (1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
     @property
     def W1(self):
-        """getter for W1"""
+        """
+        Retrieves the weight matrix for the hidden layer.
+
+        Returns:
+        numpy.ndarray
+            The weight matrix for the hidden layer.
+        """
         return self.__W1
-    
+
     @property
     def b1(self):
-        """getter for b1"""
+        """
+        Retrieves the bias vector for the hidden layer.
+
+        Returns:
+        numpy.ndarray
+            The bias vector for the hidden layer.
+        """
         return self.__b1
-    
+
     @property
     def A1(self):
-        """getter for A1"""
+        """
+        Retrieves the activated output of the hidden layer.
+
+        Returns:
+        float
+            The activated output of the hidden layer.
+        """
         return self.__A1
-    
+
     @property
     def W2(self):
-        """getter for W2"""
+        """
+        Retrieves the weight matrix for the output neuron.
+
+        Returns:
+        numpy.ndarray
+            The weight matrix for the output neuron.
+        """
         return self.__W2
-    
+
     @property
     def b2(self):
-        """getter for b2"""
+        """
+        Retrieves the bias term for the output neuron.
+
+        Returns:
+        float
+            The bias term for the output neuron.
+        """
         return self.__b2
-    
+
     @property
     def A2(self):
-        """getter for A2"""
+        """
+        Retrieves the activated output of the output neuron.
+
+        Returns:
+        float
+            The activated output of the output neuron.
+        """
         return self.__A2
