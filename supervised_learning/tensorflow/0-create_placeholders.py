37b4fdcad061a68cee24debae230b7c0d03a6aea
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-10-18 13:11:02 +0400

    documented

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""task1"""
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
 
 def create_placeholders(nx, classes):
+    """function that returns two placeholders"""
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
     return x, y
