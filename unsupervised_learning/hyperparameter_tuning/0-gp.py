81f56903d74ecb69167452fe154f2d7a6a355d60
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-12-02 21:00:45 +0400

    fix pep error

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
+"""GaussianProcess"""
 
 
 import numpy as np
+
 
 class GaussianProcess:
     """
     Represents a noiseless 1D Gaussian process.
     """
-    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, L=1, sigma_f=1):
         """
         Class constructor that initializes the Gaussian process.
         """
@@ -15,20 +17,22 @@ class GaussianProcess:
         # Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
         self.X = X_init
         self.Y = Y_init
-        self.l = l
+        self.L = L
         self.sigma_f = sigma_f
-        
         # Set the covariance kernel matrix for the initial inputs
         self.K = self.kernel(self.X, self.X)
-    
+
     def kernel(self, X1, X2):
         """
-        Computes the covariance kernel matrix between two sets of points using the RBF kernel.
+        Kernel.
         """
-        # Compute the pairwise squared distances between the points in X1 and X2
-        sqdist = np.sum(X1**2, axis=1).reshape(-1, 1) + np.sum(X2**2, axis=1) - 2 * np.dot(X1, X2.T)
-        
+        sqdist = np.sum(X1**2,
+                        axis=1).reshape(-1,
+                                        1) + np.sum(X2**2,
+                                                    axis=1) - 2 * np.dot(X1,
+                                                                         X2.T)
+
         # Compute the kernel using the RBF formula
         K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
-        
+
         return K
