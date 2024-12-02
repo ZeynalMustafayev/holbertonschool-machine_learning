9d0b4d822ff2f93d3e43299ddf24f7af10e47ca8
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-12-02 20:57:31 +0400

    fix error

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -3,33 +3,32 @@
 
 import numpy as np
 
-
 class GaussianProcess:
     """
     Represents a noiseless 1D Gaussian process.
     """
-    def __init__(self, X_init, Y_init, L=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
-        Class constructor
+        Class constructor that initializes the Gaussian process.
         """
-        # Inputs already sampled (t, 1)
-        self.X = X_init  # Inputs
-        self.Y = Y_init  # Outputs corresponding to inputs
-        self.L = L       # Length parameter for the kernel
-        self.sigma_f = sigma_f  # Standard deviation of output
-
-        # Calculate the initial covariance kernel matrix
+        # X_init is a numpy.ndarray of shape (t, 1) representing the inputs
+        # Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        
+        # Set the covariance kernel matrix for the initial inputs
         self.K = self.kernel(self.X, self.X)
-
+    
     def kernel(self, X1, X2):
         """
-        Radial Basis Function (RBF) kernel to compute covariance matrix
+        Computes the covariance kernel matrix between two sets of points using the RBF kernel.
         """
-        # Expand dimensions to allow broadcasting
-        sqdist = np.sum(X1**2,
-                        axis=1).reshape(-1,
-                                        1) + np.sum(X2**2,
-                                                    axis=1) - 2 * np.dot(X1,
-                                                                         X2.T)
+        # Compute the pairwise squared distances between the points in X1 and X2
+        sqdist = np.sum(X1**2, axis=1).reshape(-1, 1) + np.sum(X2**2, axis=1) - 2 * np.dot(X1, X2.T)
+        
+        # Compute the kernel using the RBF formula
         K = self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        
         return K
