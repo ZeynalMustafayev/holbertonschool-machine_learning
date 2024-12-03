97f80a2adc53a8bc8e75709b968475f23c6c2b14
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-12-03 22:06:46 +0400

    finally

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""BayesianOptimization"""
 
 
 import numpy as np
@@ -7,11 +8,13 @@ GP = __import__('2-gp').GaussianProcess
 
 class BayesianOptimization:
     """BayesianOptimization"""
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
         """bayesian optimization"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], num=ac_samples).reshape(-1, 1)
+        self.X_s = np.linspace(bounds[0],
+                               bounds[1], num=ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
         self.X = X_init
